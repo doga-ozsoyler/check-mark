b87ad16a38bcbae926855dac83ee17474baa76e7
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-01-06 17:16:05 +0300

    Add modal

--- client/src/Screens/Countdown.tsx
+++ client/src/Screens/Countdown.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { FC, useState } from "react";
+import { Modal, Button, View, Input, Stack, Text } from "native-base";
 
-const CountdownScreen = () => {
+const CountdownScreen: FC = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+
   return (
-    <View>
-      <Text>Countdown</Text>
+    <View flex="1" alignItems="center" justifyContent="center">
+      <Button onPress={() => setVisible(true)}>Open Modal</Button>
+      <Modal isOpen={visible} onClose={() => setVisible(false)}>
+        <Modal.Content backgroundColor="#2D3033">
+          <Modal.Body>
+            <Stack alignItems="center">
+              <Text color="#fff" fontSize="lg" mb={4} bold>
+                Enter A Number
+              </Text>
+              <Input
+                w="270px"
+                size="md"
+                keyboardType="numeric"
+                placeholder="39"
+                mb={8}
+              />
+              <Button colorScheme="teal" w="70px" borderRadius="50">
+                OK
+              </Button>
+              <Button colorScheme="teal" variant="link" borderRadius="50">
+                Cancel
+              </Button>
+            </Stack>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
     </View>
   );
 };
